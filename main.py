1879e843bf304e56ee084a738114bad246ed1887
David Lama Pulido <davidlamapul@gmail.com>
2022-07-05 18:11:25 +0200

    A

---
+++ main.py
@@ -0,0 +1,21 @@
+from sklearn import datasets, neighbors
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import make_pipeline
+import numpy
+
+def iris():
+    iris = datasets.load_iris()
+
+    X = iris.data
+    y = iris.target
+    print(X)
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.8, random_state=2020)
+
+    clf = neighbors.KNeighborsClassifier()
+    clf.fit(X_train, y_train)
+    print("TEST:", round(clf.score(X_test, y_test), 2))
+
+
+iris()
