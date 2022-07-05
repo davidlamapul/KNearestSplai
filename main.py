9339675c0913eebc7b446e9ece01dde95a07827b
David Lama Pulido <davidlamapul@gmail.com>
2022-07-05 18:40:23 +0200

    A

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ from sklearn.model_selection import train_test_split
 from sklearn.pipeline import make_pipeline
 import numpy
 
+
 def iris():
     iris = datasets.load_iris()
 
     X = iris.data
     y = iris.target
-    print(X)
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.8, random_state=2020)
 
