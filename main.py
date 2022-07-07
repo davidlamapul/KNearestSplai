8b1cb722df07cdef68f7023291a15a3f9871c4a9
David Lama Pulido <davidlamapul@gmail.com>
2022-07-07 15:42:20 +0200

    A

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import make_pipeline
 import numpy
+import pandas
 
 
 def iris():
@@ -18,4 +19,17 @@ def iris():
     print("TEST:", round(clf.score(X_test, y_test), 2))
 
 
-iris()
+def asteroids():
+    data = pandas.read_csv('data/neo_v2.csv')
+
+    data.drop(columns=['id', 'name', 'orbiting_body'], inplace=True)
+    y = data['hazardous']
+    X = data.drop(columns=['hazardous'])
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.8, random_state=2020)
+
+    clf = neighbors.KNeighborsClassifier()
+    clf.fit(X_train, y_train)
+    print("TEST:", round(clf.score(X_test, y_test), 2))
+
+asteroids()
